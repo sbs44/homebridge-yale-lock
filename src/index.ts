d0d815311401403d39502c92edcaf92c2f466659
Spencer <sbsargent@pitt.edu>
2024-10-30 16:34:50 -0400

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { API } from 'homebridge';
+import type { API } from 'homebridge';
+
 import { SeamLockPlatform } from './platform.js';
 import { PLATFORM_NAME } from './settings.js';
 
+/**
+ * This method registers the platform with Homebridge
+ */
 export default (api: API) => {
   api.registerPlatform(PLATFORM_NAME, SeamLockPlatform);
 };
